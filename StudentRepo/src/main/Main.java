8cd59b8bdae11cd98be41a2855ca78fbf916a753
Subrata Roy <subrataxon@gmail.com>
2018-07-24 03:55:32 +0600

    ok

--- StudentRepo/src/main/Main.java
+++ StudentRepo/src/main/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("../fxml/main.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Student Repo");
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
